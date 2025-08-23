7a975e76a5d47b6f91719bcadcd5f6b05c76569c
mohousch <mohousch@gmail.com>
2025-08-23 17:16:39 +0200

    - Update Build.

--- prepare-for-bs.sh
+++ prepare-for-bs.sh
@@ -89,8 +89,7 @@ PACKAGES="\
 	${UBUNTU:+rpm}                                               ${FEDORA:+rpm-build}      ${GENTOO:+rpm}         \
 	${UBUNTU:+lsb-release}          ${SUSE:+lsb-release}         ${FEDORA:+redhat-lsb}     ${GENTOO:+lsb-release} \
 	${UBUNTU:+git}                  ${SUSE:+git-core}            ${FEDORA:+git}            ${GENTOO:+git}         \
-	${UBUNTU:+libncurses5-dev}      ${SUSE:+ncurses-devel}       ${FEDORA:+ncurses-devel}  ${GENTOO:+ncurses}     \
-	${UBUNTU:+libncursesw5-dev}                                                                                   \
+	${UBUNTU:+libncurses-dev}      ${SUSE:+ncurses-devel}       ${FEDORA:+ncurses-devel}  ${GENTOO:+ncurses}      \
 	${UBUNTU:+gettext}              ${SUSE:+gettext-devel}       ${FEDORA:+gettext-devel}  ${GENTOO:+gettext}     \
 	${UBUNTU:+zlib1g-dev}           ${SUSE:+zlib-devel}          ${FEDORA:+zlib-devel}     ${GENTOO:+zlib}        \
 	${UBUNTU:+g++}                  ${SUSE:+gcc gcc-c++}         ${FEDORA:+gcc-c++}                               \
@@ -117,7 +116,6 @@ PACKAGES="\
 	${UBUNTU:+mtools}                                                                                             \
 	${UBUNTU:+u-boot-tools}                                                                                       \
 	${UBUNTU:+android-sdk-libsparse-utils}                                                                        \
-	${UBUNTU:+libpcre3-dev}											      \
 	${UBUNTU:+lua5.2}                                                                                             \
 	${UBUNTU:+liblua5.2-dev}                                                                                      \
 	${UBUNTU:+libcap-dev}
